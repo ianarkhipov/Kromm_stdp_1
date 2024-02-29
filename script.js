8118643201068ebdfc49578c3a04733625db2d17
Ian.arkhipov <85026167+interiorengine@users.noreply.github.com>
2024-02-29 16:05:09 +0300

    Stage 2 Removed div insert from addElement function, added counter

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 
 
+
+let div_1 = document.createElement("div")
+document.body.insertBefore(div_1, document.getElementById("target_div1"));
+
+let counter = 1;
+
 document.getElementById("pInput").addEventListener("click", addElement);
 
-
-let div_1 = (newDiv = null);
-
 function addElement() {
-  let newDiv = document.createElement("div")
-  newDiv.innerHTML = "<p>Hello, world!</p>";
+  let newParagraph = document.createElement("p")
+  newParagraph.textContent = "Hello, world! " + counter;
+  newParagraph.id = "p_" + counter;
+  div_1.appendChild(newParagraph);
 
-div_1 = document.getElementById("target_div1");
-document.body.insertBefore(newDiv, div_1)
+  counter++;
 }
 
 
