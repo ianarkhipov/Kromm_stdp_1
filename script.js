acdc4e7742dab9142ddb3189e7fcac10e2ce6b8c
Vladimir Kromm <vladimir.kromm@mercedes-benz.com>
2024-02-29 16:49:47 +0100

    second task changes from Vladimir

--- script.js
+++ script.js
@@ -1,19 +1,14 @@
 
-
-
-let div_1 = document.createElement("div")
-document.body.insertBefore(div_1, document.getElementById("target_div1"));
-
+const targetDiv = document.getElementById("target_div")
 let counter = 1;
 
-document.getElementById("pInput").addEventListener("click", addElement);
+document.getElementById("add_button").addEventListener("click", addElement);
 
 function addElement() {
   let newParagraph = document.createElement("p")
   newParagraph.textContent = "Hello, world! " + counter;
   newParagraph.id = "p_" + counter;
-  div_1.appendChild(newParagraph);
-
+  targetDiv.appendChild(newParagraph);
   counter++;
 }
 
