778289f01a2262931eb146dbd00a98f27ed2e37e
Vladimir Kromm <vladimir.kromm@mercedes-benz.com>
2024-02-29 17:18:54 +0100

    3 and 4 points done

--- script.js
+++ script.js
@@ -1,15 +1,41 @@
 
 const targetDiv = document.getElementById("target_div")
-let counter = 1;
+let counter = 0;
 
 document.getElementById("add_button").addEventListener("click", addElement);
 
 function addElement() {
-  let newParagraph = document.createElement("p")
-  newParagraph.textContent = "Hello, world! " + counter;
-  newParagraph.id = "p_" + counter;
-  targetDiv.appendChild(newParagraph);
   counter++;
+  const newInnerElement = createInnerElement(counter)
+  targetDiv.appendChild(newInnerElement);
 }
 
+function createInnerElement(idx){
+  const newElement = document.createElement("div")
+  newElement.style.margin = '10px';
+  newElement.id = `inner_element_${idx}`
 
+  const paragraph = document.createElement("p")
+  paragraph.textContent = "Hello, world! " + idx;
+
+
+  const button = document.createElement("button")
+  button.innerText = "Remove"
+  const myIdxRemover = () => { removeInnerElement(idx) }
+  button.addEventListener("click", myIdxRemover)
+  // option 2
+  // button.addEventListener("click", ()=> {
+  //   document.querySelector(`#inner_element_${idx}`).remove()
+  // })
+
+  newElement.appendChild(paragraph)
+  newElement.appendChild(button)
+  return newElement
+}
+
+function removeInnerElement(idx) {
+  document.querySelector(`#inner_element_${idx}`).remove()
+  if (idx === counter) {
+    counter--
+  }
+}
