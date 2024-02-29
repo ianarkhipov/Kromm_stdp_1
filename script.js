cb991f5166c74adf0a5f8b896d167458a5b12f08
Ian.arkhipov <85026167+interiorengine@users.noreply.github.com>
2024-02-29 11:49:00 +0300

    Stage 1 fix

--- script.js
+++ script.js
@@ -2,12 +2,12 @@
 
 document.getElementById("pInput").addEventListener("click", addElement);
 
-//document.body.onload = addElement;
+
 let div_1 = (newDiv = null);
 
 function addElement() {
   let newDiv = document.createElement("div")
-  newDiv.innerHTML = "<h3>Hello, world!</h3>";
+  newDiv.innerHTML = "<p>Hello, world!</p>";
 
 div_1 = document.getElementById("target_div1");
 document.body.insertBefore(newDiv, div_1)
