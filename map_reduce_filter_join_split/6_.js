87253be2b0c2dc7a27c7c2d50784990693cfa016
Ian.arkhipov <85026167+interiorengine@users.noreply.github.com>
2024-03-21 16:00:22 +0300

    ongoing task 6

--- map_reduce_filter_join_split/6_.js
+++ map_reduce_filter_join_split/6_.js
@@ -9,5 +9,15 @@
 const students = [
   { name: "Alice", scores: [90, 85, 97] },
   { name: "Bob", scores: [75, 80] },
+  { name: "Carl", scores: [45, 56, 94]},
 ];
 
+const median = arr => {
+  const sorted = arr.slice().sort((a, b) => a - b);
+  const middle = Math.floor(sorted.length / 2);
+  if (sorted.length % 2 === 0) {
+    return (sorted[middle - 1] + sorted[middle]) / 2;
+} else {
+  return sorted[middle];
+}
+};
