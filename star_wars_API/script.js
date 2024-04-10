8232a06abe52f8dbc9716f52b26ed3ff943cd32c
Ian.arkhipov <85026167+interiorengine@users.noreply.github.com>
2024-04-10 15:43:04 +0300

    SWAPI name, height, weight scroll through completed

--- star_wars_API/script.js
+++ star_wars_API/script.js
@@ -1,12 +1,30 @@
+let currentCharacterIndex = 1;
+const textElement = document.getElementById("text");
 
+function fetchCharacter(index) {
+  let request = `https://swapi.dev/api/people/${index}/`
 
-let request = "https://swapi.dev/api/people/1/"
+  fetch(request).then((response) => {
+    return response.json();
+  }).then((data) => {
+    let p = document.getElementById("text");
+    console.log(data);
+    p.innerHTML = `${data.name}, height: ${data.height}, weight: ${data.mass}`;
+  });
+}
 
-fetch(request).then((response) => {
-  return response.json();
-}).then( (data) => {
-  let p = document.getElementById("text");
-  console.log(data);
-  p.innerHTML = `${data.name}, height: ${data.height}, weight: ${data.mass}`;
-})
+//вспомогательное уравнивание индекса?
+fetchCharacter(currentCharacterIndex);
+
+document.getElementById("prev").addEventListener("click", () => {
+  if (currentCharacterIndex > 1) {
+    currentCharacterIndex--;
+    fetchCharacter(currentCharacterIndex);
+  }
+});
+  document.getElementById("next").addEventListener("click", () => {
+      currentCharacterIndex++;
+      fetchCharacter(currentCharacterIndex);
+});
+
 
