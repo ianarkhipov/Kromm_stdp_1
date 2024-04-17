9f6aa2494629382b133cc1dee90f458f5b421d49
Ian.arkhipov <85026167+interiorengine@users.noreply.github.com>
2024-04-17 12:46:00 +0300

    Button increase

--- React_swapi/react-sw/src/App.js
+++ React_swapi/react-sw/src/App.js
@@ -5,11 +5,16 @@ import {useState} from "react";
 function App() {
   const [myState] = useState(101)
 
+  const increase = ()=> {
+    console.log("I am increased")
+  }
+
   return (
     <div className="App">
      <p>
        This is my React app. It has state: {myState}
      </p>
+      <button onClick={increase}>Add</button>
     </div>
   );
 }
