2174a1ec1fc866cb60e545f36dd69a28df0210f7
Ian.arkhipov <85026167+interiorengine@users.noreply.github.com>
2024-04-22 16:27:41 +0300

    Catches and manages !props.id now

--- react-sw/src/Character.jsx
+++ react-sw/src/Character.jsx
@@ -3,15 +3,26 @@ import {useEffect, useState} from "react";
 
 function Character(props) {
     const [character, setCharacter] = useState({})
+    const [error, setError] = useState(null)
 
 const fetchCharacter = () => {
+        if (!props.id) {
+            setError('Failed to fetch');
+            return;
+        }
+
     fetch(`https://swapi.tech/api/people/${props.id}/?format=json`)
         .then((res) =>{(res.json())
             .then((data) => {
                 //структура запроса
                 console.log(data.result);
                 setCharacter(data.result.properties);
+                setError(null);
             })
+            .catch((error) => {
+                setError('There is no such a character')
+
+            });
         })
 }
 
@@ -19,6 +30,10 @@ const fetchCharacter = () => {
         fetchCharacter,
         [props.id]
     );
+
+    if (error) {
+        return <div>Error: {error}</div>
+    }
 
     return (
         <div>
